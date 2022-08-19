018e0eb02f1357068ed481a970f96010dcbbdaf4
Adebayo Temitope <adecancode@gmail.com>
2022-08-20 00:33:20 +0100

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 };
